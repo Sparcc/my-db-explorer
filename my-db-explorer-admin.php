9dd3eb6de957d60613b3063122dfdfca3adcaf93
avih8r <sparco.209@gmail.com>
2018-02-01 16:22:53 +1100

    added proof of concept ajax junk

--- my-db-explorer-admin.php
+++ my-db-explorer-admin.php
@@ -6,20 +6,23 @@
     <?php do_settings_sections( 'my-plugin-settings-group' ); ?>
     <table class="form-table">
         <tr valign="top">
-        <th scope="row">Field 1</th>
-        <td><input type="text" name="accountant_name" /></td>
+        <th scope="row">Add Field</th>
+        <td><input type="text" name="field" /></td>
+        <td><button type="button">Add</button></td>
         </tr>
          
         <tr valign="top">
-        <th scope="row">Field 2</th>
-        <td><input type="text" name="accountant_phone" /></td>
+        <th scope="row">Data Type</th>
+        <td><input type="text" name="dataType" /></td>
         </tr>
         
         <tr valign="top">
-        <th scope="row">Field 3</th>
+        <th scope="row">Form Input</th>
         <td><input type="text" name="accountant_email" /></td>
         </tr>
     </table>
+    
+<h2>Existing Fields</h2>
     
     <?php submit_button(); ?>
 
